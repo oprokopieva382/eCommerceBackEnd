8f1b06a1a0479ba9807ad84d8690a2db6918aafe
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-01 22:21:25 -0500

    add get one product request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,42 +1,60 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
-try {
-const productData = await Product.findAll({
-  include: [
-    {
-      model: Category,
-      attributes: ["id", "category_name"],
-    },
-    {
-      model: Tag,
-      attributes: ["id", "tag_name"],
-    },
-  ],
-});
- productData
-   ? res.status(200).json(productData)
-   : res.status(404).json({ Error: "Products not found" });
-}catch(err) {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ["id", "category_name"],
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_name"],
+        },
+      ],
+    });
+    productData
+      ? res.status(200).json(productData)
+      : res.status(404).json({ Error: "Products not found" });
+  } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
-}
-  
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", async ({ params: { id } }, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(id, {
+      include: [
+        {
+          model: Category,
+          attributes: ["id", "category_name"],
+        },
+        {
+          model: Tag,
+          attributes: ["id", "tag_name"],
+        },
+      ],
+    });
+    productData
+      ? res.status(200).json(productData)
+      : res.status(404).json({ Error: "Products not found" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -68,7 +86,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -77,9 +95,8 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-
         ProductTag.findAll({
-          where: { product_id: req.params.id }
+          where: { product_id: req.params.id },
         }).then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
@@ -112,7 +129,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
 });
 
