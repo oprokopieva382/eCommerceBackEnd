e82bd0cbb90666f4ab676815f27b0c16077f5324
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-02 17:51:52 -0500

    create get all and get by id tag requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,45 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({ include: Product });
+    tagData
+      ? res.status(200).json(tagData)
+      : res.status(404).json({ Error: "Tag not found" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async ({ params: { id }}, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await tagData.findByPk(id, { include: Product });
+    tagData
+      ? res.status(200).json(tagData)
+      : res.status(404).json({ Error: "Tag with such id was not found" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
